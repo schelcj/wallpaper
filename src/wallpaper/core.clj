1a33d38e1572bc1818ccf02ab6132c97a0011e36
Chris Scheller <schelcj@pobox.com>
2025-08-13 17:26:32 -0400

    adding command line opt support or trying to at least

--- src/wallpaper/core.clj
+++ src/wallpaper/core.clj
@@ -1,50 +1,20 @@
 (ns wallpaper.core
+  (:require [clojure.tools.cli :refer [parse-opts]])
   (:require [clojure.edn :as edn])
-  (:require [clojure.java.io :as io]))
-
-;; TODO - getopts arguments to implement
-;; (don't know how getopts works yet in clojure)
-;;
-;;    -c, --category    Wallpaper category
-;;    -f, --flush-cache Flush the wallpaper cache
-;;    -d, --dump-cache  Dump the current wallpaper cache to STDOUT
-;;    -l, --lock        Lock the current wallpaper
-;;    -u, --unlock      Unlock the current wallpaper
-;;        --clear       Clear the previous wallpaper category
-;;    -p, --previous    Set the wallpaper to the previous paper
-;;    -i, --image       Set image as the current wallpaper
+  (:require [clojure.java.io :as io])
+  (:gen-class))
 
 (def prefix (io/file (System/getenv "HOME") ".wallpapers"))
 
-(def config {
-  :lock-file (io/file prefix "lock")
-  :category-file (io/file prefix "category")
-  :wallpapers-dir (io/file prefix "Wallpapers")
-  :current (io/file prefix "current")
-  :previous (io/file prefix "previous")
-  :history (io/file prefix "history.edn")
-  :sources (io/file prefix "sources")
-  :default-category "all"
-})
+(def config {:lock-file (io/file prefix "lock")
+             :category-file (io/file prefix "category")
+             :wallpapers-dir (io/file prefix "Wallpapers")
+             :current (io/file prefix "current")
+             :previous (io/file prefix "previous")
+             :history (io/file prefix "history.edn")
+             :sources (io/file prefix "sources")
+             :default-category "all"})
 
-;; TODO
-;; - if the lock file exists exit making no changes
-;; - build list of directories to search for wallpapers
-;;   - could be a single directory if passed a category via getopt
-;;   - could be a single directory if category file exists and directory exists
-;;   - if no arg and no file use contents of the default :sources file
-;; - build list of wallpapers in the directories built previously
-;; - load history of previously displayed wallpapers
-;; - filter out previously displayed from the list of wallpapers to create list of candidates
-;; - apply weights to the list of wallpapers according to ctime of the wallpaper to build new list
-;; - select random wallpaper from the filtered and weighted list
-;; - use setter to display wallpaper
-;; - rename the current file to previous file
-;; - add current wallpaper to current file
-;; - add wallpaper to the history
-;; - record new history to disk
-;; - exit
-;;
 ;; Optional arguments
 ;; - category: passed update category file on disk then let normal flow happen
 ;; - clear: delete the category file on disk then let normal flow happen
@@ -54,13 +24,40 @@
 ;; - previous: read contents of previous file, set the wallpaper to this wallpaper, exit
 ;; - image: set the wallpaper to the given file path and exit
 ;; - dump-cache: print contents of the history to STDOUT and exit
-;;
-(defn sources
+(def cli-options
+  [["-c" "--category" "Wallpaper category"
+    :default (:default-category config)
+    :parse-fn str]
+   ["-f" "--flush-cache" "Flush the wallpaper history cache"]
+   ["-d" "--dump-cache" "Print current wallpaper history cache to STDOUT"]
+   ["-l" "--lock" "Lock the current wallpaper"]
+   ["-u" "--unlock" "Unlock the current wallpaper"]
+   ["-p" "--previous" "Set the wallpaper to the previous image"]
+   ["-i" "--image" "Set the provided image as the current wallpaper"]
+   ["-t" "--tile" "Tiled the provided image as the wallpaper"]
+   ["-r" "--clear" "Clear the previous wallpaper category"]
+   ["-h" "--help" "Show help"]])
+
+(defn usage [options-summary]
+  (->> ["wallpaper"
+        ""
+        "Usage: wallpaper [options]"
+        ""
+        "Options:"
+        options-summary]
+       (clojure.string/join \newline)))
+
+(defn categories
   [& category]
   (if category
     (vec category)
     (with-open [r (io/reader (:sources config))]
       (vec (line-seq r)))))
+
+(defn load-history
+  "Read the history of previously displayed wallpapers."
+  []
+  (edn/read-string (slurp (:history config))))
 
 (defn wallpaper-dirs
   "Build a deq of all the directories to search for wallpapers in.
@@ -102,7 +99,28 @@
   [wallpapers]
   (spit (:history config) (pr-str wallpapers)))
 
-(defn load-history
-  "Read the history of previously displayed wallpapers."
-  []
-  (edn/read-string (slurp (:history config))))
+;; TODO
+;; - if the lock file exists exit making no changes
+;; - build list of directories to search for wallpapers
+;;   - could be a single directory if passed a category via getopt
+;;   - could be a single directory if category file exists and directory exists
+;;   - if no arg and no file use contents of the default :sources file
+;; - build list of wallpapers in the directories built previously
+;; - load history of previously displayed wallpapers
+;; - filter out previously displayed from the list of wallpapers to create list of candidates
+;; - apply weights to the list of wallpapers according to ctime of the wallpaper to build new list
+;; - select random wallpaper from the filtered and weighted list
+;; - use setter to display wallpaper
+;; - rename the current file to previous file
+;; - add current wallpaper to current file
+;; - add wallpaper to the history
+;; - record new history to disk
+;; - exit
+;;
+(defn -main
+  [& args]
+  (let [{:keys [options arguments errors summary]} (parse-opts args cli-options)]
+    (cond
+      (:help options)
+      (do (println (usage summary))
+          (System/exit 0)))))
