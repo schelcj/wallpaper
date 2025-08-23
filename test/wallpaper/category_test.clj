95abb6b96c3fe7c3211683b1b6934617865c43dc
Chris Scheller <schelcj@pobox.com>
2025-08-23 17:39:50 -0400

    working on tests for the category namespace

--- test/wallpaper/category_test.clj
+++ test/wallpaper/category_test.clj
@@ -1,3 +1,30 @@
 (ns wallpaper.category-test
-  (:require [wallpaper.category :as sut]
-            [clojure.test :as t]))
+  (:require [clojure.java.io :as io])
+  (:require [clojure.string :as string])
+  (:require [wallpaper.category :as cat]
+            [clojure.test :refer :all]))
+
+(import '[java.io File])
+
+(def category-file (File/createTempFile "test-category-" ".edn"))
+(def sources-file (-> "resources/fixtures/sources" io/resource))
+(def categories (string/split-lines (slurp sources-file)))
+
+;; pass file and optional category
+(deftest test-all
+  (testing "Loading all categories")
+  (is (= (count categories) (count (cat/all sources-file)))))
+
+;; pass file and category
+;; can recored category
+;; file exists and is non-zero length
+(deftest test-set
+  (testing "Setting a catgory"))
+
+;; pass file
+;; can clear category
+;; file does not exist
+(deftest test-clear
+  (testing "Clearing a set category"))
+
+(.deleteOnExit category-file)
