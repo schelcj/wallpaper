fb6ea8772b51ffd0716a8678dd281b80a73a5097
Chris Scheller <schelcj@pobox.com>
2026-06-20 08:05:47 -0400

    move stats display to the stats namespace

--- src/wallpaper/stats.clj
+++ src/wallpaper/stats.clj
@@ -4,6 +4,7 @@
   (:require [wallpaper.history :as history])
   (:require [wallpaper.papers :as papers])
   (:require [clojure.java.io :as io])
+  (:require [clojure.pprint :refer [print-table]])
   (:gen-class))
 
 (defn total-wallpapers-available
@@ -43,4 +44,28 @@
    :total-wallpapers-available-by-category (total-wallpapers-available-by-category)
    :total-wallpapers-displayed-by-category (total-wallpapers-displayed-by-category)})
 
-
+(defn print-stats!
+  ""
+  []
+  (let [totals (overall)
+        available (:total-wallpapers-available-by-category totals)
+        displayed (:total-wallpapers-displayed-by-category totals)
+        summary   (for [[cat total] available
+                        :let [shown (get displayed cat 0)
+                              remain (- total shown)
+                              pct (double (* 100 (/ shown total)))]]
+                    {:category cat
+                     :available total
+                     :displayed shown
+                     :remaining remain
+                     :percent (format "%.1f%%" pct)})
+        total-available (:total-wallpapers-available totals)
+        total-displayed (:total-wallpapers-displayed totals)
+        totals {:category "TOTAL"
+                :available total-available
+                :displayed total-displayed
+                :remaining (- total-available total-displayed)
+                :percent (format "%.1f%%" (double (* 100 (/ total-displayed total-available))))}
+        separator {:category "----" :available "" :displayed "" :remaining "" :percent ""}
+        results (conj (vec (reverse (sort-by :percent summary))) separator totals)]
+    (print-table [:category :available :displayed :remaining :percent] results)))
